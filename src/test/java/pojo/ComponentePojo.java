d41148d311741584e2af8ee54018049a48afa709
MatheusLuisCunha <66577895+MatheusLuisCunha@users.noreply.github.com>
2021-08-23 09:34:00 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/pojo/ComponentePojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class ComponentePojo {
+    private String componenteNome;
+    private double componenteQuantidade;
+
+    public String getComponenteNome() {
+        return componenteNome;
+    }
+
+    public void setComponenteNome(String componenteNome) {
+        this.componenteNome = componenteNome;
+    }
+
+    public double getComponenteQuantidade() {
+        return componenteQuantidade;
+    }
+
+    public void setComponenteQuantidade(double componenteQuantidade) {
+        this.componenteQuantidade = componenteQuantidade;
+    }
+}
